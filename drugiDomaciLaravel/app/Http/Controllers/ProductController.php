3c273771b71220ca4f9f6253106ecb9407b2139a
MarkoBojanic18 <markobojanic18@gmail.com>
2021-12-13 19:20:30 +0100

    cart has been started

--- drugiDomaciLaravel/app/Http/Controllers/ProductController.php
+++ drugiDomaciLaravel/app/Http/Controllers/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Cart;
+
 
 class ProductController extends Controller
 {
@@ -12,9 +14,27 @@ class ProductController extends Controller
         $data = Product::all();
         return view('product',['products'=>$data]);
     }
+    
     function detail($id)
     {
         $data =Product::find($id);
         return view('detail',['product'=>$data]);
+    }
+
+    function addToCart(Request $req)
+    {
+        if($req->session()->has('user'))
+        {
+           $cart= new Cart;
+           $cart->user_id=$req->session()->get('user')['id'];
+           $cart->product_id=$req->product_id;
+           $cart->save();
+           return redirect('/');
+
+        }
+        else
+        {
+            return redirect('/login');
+        }
     }
 }

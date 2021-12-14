6313e3bdae326b1863a9d6a315e4ecacf8e7fbeb
MarkoBojanic18 <markobojanic18@gmail.com>
2021-12-14 10:44:02 +0100

    Just space was removed

--- drugiDomaciLaravel/routes/web.php
+++ drugiDomaciLaravel/routes/web.php
@@ -31,7 +31,6 @@ Route::get("/",[ProductController::class,'index']);
 Route::get("detail/{id}",[ProductController::class,'detail']);
 Route::get("cartlist",[ProductController::class,'cartList']);
 Route::post("add_to_cart",[ProductController::class,'addToCart']);
-
 Route::get("removecart/{id}",[ProductController::class,'removeCart']);
 Route::get("ordernow",[ProductController::class,'orderNow']);
 Route::post("orderplace",[ProductController::class,'orderPlace']);
